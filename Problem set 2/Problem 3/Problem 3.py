832284dffeb7dfbaf3ee028100dc5b6d87aff3b2
gregorysamuel <71037928+gregorysamuel@users.noreply.github.com>
2020-09-24 22:10:26 -0400

    Add files via upload

---
+++ Problem set 2/Problem 3/Problem 3.py
@@ -0,0 +1,42 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import integrate
+
+half_life=[]
+half_life.append(4.468e9*365.25*24*60*60)
+half_life.append(24.10*24*60*60)
+half_life.append(6.7*60*60)
+half_life.append(245500*365.25*24*60*60)
+half_life.append(75830*365.25*24*60*60)
+half_life.append(1600*365.25*24*60*60)
+half_life.append(3.8235*24*60*60)
+half_life.append(3.1*60)
+half_life.append(26.8*60)
+half_life.append(19.9*60)
+half_life.append(164.3e-6)
+half_life.append(22.3*365.25*24*60*60)
+half_life.append(5.015*365.25*24*60*60)
+half_life.append(138.376*24*60*60)
+
+def fun(t,N,half_life=half_life):
+    dNdt=np.zeros(len(half_life)+1)
+    dNdt[0]=-N[0]/half_life[0]
+    for i in range(len(dNdt)-2):
+        dNdt[i+1]=(N[i]/half_life[i]) - N[i+1]/half_life[i+1]
+    dNdt[-1]=N[-2]/half_life[-2]
+    return dNdt
+
+N0=np.zeros(len(half_life)+1)
+N0[0]=1e23
+t0,t1=0,4.468e9*365.25*24*60*60
+ans_stiff= integrate.solve_ivp(fun,[t0,t1],N0,method='Radau')
+ur238_evol=ans_stiff.y[0,:]
+ur234_evol=ans_stiff.y[3,:]
+th_evol=ans_stiff.y[4,:]
+pb_evol=ans_stiff.y[-1,:]
+time=ans_stiff.t
+
+plt.plot(time[90:95],ur234_evol[90:95],'r')
+plt.plot(time[90:95],th_evol[90:95],'g')
+#plt.xscale('log')
+plt.yscale('log')
